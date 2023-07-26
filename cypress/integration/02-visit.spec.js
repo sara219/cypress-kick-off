01ac852d1d5d8a6e59c1ee4cb306365fd1511e8c
sara219 <sara97abuawwad@gmail.com>
2023-07-26 14:42:23 +0300

    add new argument as query

--- cypress/integration/02-visit.spec.js
+++ cypress/integration/02-visit.spec.js
@@ -1,8 +1,10 @@
-
 it('Should visit the website correctly', () => {
   // with visit command we can go to a website, or localhost or file using file path
   // cypress recommend to set a baseUrl is cypress.json
-  cy.visit('/')
+  // we can add second argument, this argument is object.
+  cy.visit('/search', {
+    qs: {
+      q: 'cypress',
+    },
+  })
 })
-
-
