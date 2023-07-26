eb7c71275ead265a0dbdf2f2d9aa2b2ffc516134
sara219 <sara97abuawwad@gmail.com>
2023-07-27 00:46:47 +0300

    view port command

---
+++ cypress/integration/03-viewport.spec.js
@@ -0,0 +1,6 @@
+it('should be able to open the website in different views(screen)', () => {
+  // use cy.viewport to set the width and height of the the test runner
+  // its usefully for testing out application for different view port or different screen size
+  cy.viewport(550, 750)
+  cy.visit('cypress/index.html')
+})
