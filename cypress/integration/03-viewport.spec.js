ac04f58c4bf1fad8c09f2b1461a1b3841a5aa80d
sara219 <sara97abuawwad@gmail.com>
2023-07-27 01:06:25 +0300

    pre-defined viewport

--- cypress/integration/03-viewport.spec.js
+++ cypress/integration/03-viewport.spec.js
@@ -3,3 +3,10 @@ it('should be able to open the website in different views(screen)', () => {
   // its usefully for testing out application for different view port or different screen size
   cy.visit('cypress/index.html')
 })
+
+// test present dimension 'pre-defined viewport' :: https://docs.cypress.io/api/commands/viewport#__docusaurus_skipToContent_fallback
+it('Should be able to render on iphone, samsung and macbook ', () => {
+  cy.viewport('iphone-6', 'landscape')
+  cy.viewport('macbook-15')
+  cy.viewport('samsung-s10')
+})
