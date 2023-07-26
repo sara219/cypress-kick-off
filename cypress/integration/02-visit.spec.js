232c33c401f78ede37b60ee912ab2918931cd377
sara219 <sara97abuawwad@gmail.com>
2023-07-26 13:12:17 +0300

    write test case for visit a website

---
+++ cypress/integration/02-visit.spec.js
@@ -0,0 +1,8 @@
+
+it('Should visit the website correctly', () => {
+  // with visit command we can go to a website, or localhost or file using file path
+  // cypress recommend to set a baseUrl is cypress.json
+  cy.visit('/')
+})
+
+
