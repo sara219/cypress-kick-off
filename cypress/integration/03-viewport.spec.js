0473113a18f715ab6cc08cf022f7f0ed29551299
sara219 <sara97abuawwad@gmail.com>
2023-07-27 01:25:53 +0300

    write one test for all viewport

--- cypress/integration/03-viewport.spec.js
+++ cypress/integration/03-viewport.spec.js
@@ -1,12 +1,11 @@
-it('should be able to open the website in different views(screen)', () => {
-  // use cy.viewport to set the width and height of the the test runner
-  // its usefully for testing out application for different view port or different screen size
-  cy.visit('cypress/index.html')
-})
+// use cy.viewport to set the width and height of the the test runner
+// its usefully for testing out application for different view port or different screen size
 
 // test present dimension 'pre-defined viewport' :: https://docs.cypress.io/api/commands/viewport#__docusaurus_skipToContent_fallback
-it('Should be able to render on iphone, samsung and macbook ', () => {
-  cy.viewport('iphone-6', 'landscape')
-  cy.viewport('macbook-15')
-  cy.viewport('samsung-s10')
+
+['iphone-6', 'macbook-15'].forEach((viewport) => {
+  it('Should be able to open in different viewport', () => {
+    cy.visit('cypress/index.html')
+    cy.viewport(viewport)
+  })
 })
