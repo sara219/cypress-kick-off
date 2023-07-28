200dbb41edefedb3ce28d6e3cf85a44e9590e963
sara219 <sara97abuawwad@gmail.com>
2023-07-28 12:49:15 +0300

    get element by ID  using get command

--- cypress/integration/04-getElement.spec.js
+++ cypress/integration/04-getElement.spec.js
@@ -3,11 +3,20 @@
 // We use the (get) command to find element(s)
 
 // we can find element by tag name
-
 it('Find Element by tag name', () => {
   cy.visit('cypress/index.html')
 
   // we put the tag name (html tag) inside the get command
   // The get command will return all the elements that matches the tag name
   cy.get('h2')
+})
+
+// ==============
+
+// we can find element by ID
+it.only('Find Element by ID', () => {
+  cy.visit('cypress/index.html')
+
+  // we put the ID inside the get command using #
+  cy.get('#courses')
 })
