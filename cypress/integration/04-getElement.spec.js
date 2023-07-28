455ef44b91a1f7dff08b638734965b661717da5c
sara219 <sara97abuawwad@gmail.com>
2023-07-28 13:05:29 +0300

    get element by attribute  using get command

--- cypress/integration/04-getElement.spec.js
+++ cypress/integration/04-getElement.spec.js
@@ -11,12 +11,36 @@ it('Find Element by tag name', () => {
   cy.get('h2')
 })
 
-// ==============
+// ?============================================
 
 // we can find element by ID
-it.only('Find Element by ID', () => {
+it('Find Element by ID', () => {
   cy.visit('cypress/index.html')
 
   // we put the ID inside the get command using #
+  // return the first match
   cy.get('#courses')
+})
+
+// ?============================================
+
+// we can find element by ClassName
+it('Find Element by class name', () => {
+  cy.visit('cypress/index.html')
+
+  // we put the class name inside the get command using .
+  // return all the elements that matches the class name
+  cy.get('.button-container')
+  // to specify the return element
+  cy.get('.list1.web')
+})
+
+// ?============================================
+
+// we can find element by Attribute
+it.only('Find Element by Attribute', () => {
+  cy.visit('cypress/index.html')
+  // to find any element using any attribute we use []
+  cy.get('[value="Developer"]')
+  cy.get('[type="button"]')
 })
