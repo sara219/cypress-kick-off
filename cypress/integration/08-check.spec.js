008e968fb5e3675484f9e6231335b6af7970d680
sara219 <sara97abuawwad@gmail.com>
2023-07-29 00:19:01 +0300

    cypress action - check command

---
+++ cypress/integration/08-check.spec.js
@@ -0,0 +1,12 @@
+it('check command', () => {
+    cy.visit('cypress/index.html');
+    cy.get('#Nuts').check();
+    cy.get('#Nuts').uncheck();
+    cy.get('#Manager').check();
+  
+    // We can check a checkbox or a radio button using the check command
+    // We can uncheck the checkbox using the uncheck command
+    // We can force the checking by adding an option.
+    // .check() requires the element to have type checkbox or radio
+    // .check() will automatically wait for the element to reach an actionable state
+  });
