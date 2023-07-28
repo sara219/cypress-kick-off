428a88c287d8f502498a7a111815c792abbfa289
sara219 <sara97abuawwad@gmail.com>
2023-07-28 18:22:01 +0300

    cypress action - click command

---
+++ cypress/integration/05-click.spec.js
@@ -0,0 +1,11 @@
+it('Click Command', () => {
+  cy.visit('cypress/index.html')
+
+  // We can click on an element using the click command
+  // We can only click on one element.
+  cy.get('.invoice-button').last().click()
+  // We can click on multiple elements if we add {multiple: true}
+  cy.get('.invoice-button').click({ multiple: true })
+  // We can click on hidden, or covered elements if we add {force: true}
+  cy.get('.hidden-button').click({ force: true })
+})
