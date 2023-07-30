9b897bed9d2004e28890a0a34dc5c2238feeca90
sara219 <sara97abuawwad@gmail.com>
2023-07-31 00:23:25 +0300

    cypress its command

---
+++ cypress/integration/20-its.spec.js
@@ -0,0 +1,15 @@
+/// <reference types="cypress" />
+
+it('Wrap Command', () => {
+    const student = {
+      fName: 'Sara',
+      family: 'Abu Awwad',
+      isQA: true,
+      isWatching: function () {
+        console.log('He is Watching')
+      },
+    }
+    cy.visit('cypress/index.html')
+    cy.wrap(student).its("fName").should('eql', 'Sara')
+  })
+  
